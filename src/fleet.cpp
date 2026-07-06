7b89ecdeb75b7547ba075396ba87d103818aa547
moses-maroun <mosesmaroun@gmail.com>
2026-07-06 08:01:04 +0300

    feat: implement Fleet::find (throws if not found)

--- src/fleet.cpp
+++ src/fleet.cpp
@@ -11,3 +11,10 @@ void Fleet::add(std::shared_ptr<Robot> robot) {
 void Fleet::remove(const std::string& id) {
     robots_.erase(id);
 }
+
+std::shared_ptr<Robot> Fleet::find(const std::string& id) const {
+    auto it = robots_.find(id);
+    if (it == robots_.end())
+        throw std::runtime_error("Robot not found: " + id);
+    return it->second;
+}

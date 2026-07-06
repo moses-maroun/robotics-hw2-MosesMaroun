36d878e414fcc7b4b0e28296d814356dc9ab9c2a
moses-maroun <mosesmaroun@gmail.com>
2026-07-06 07:41:33 +0300

    feat: implement Task::operator<<

--- src/task.cpp
+++ src/task.cpp
@@ -7,4 +7,10 @@ bool Task::operator<(const Task& other) const {
     return priority < other.priority;
 }
 
+std::ostream& operator<<(std::ostream& os, const Task& t) {
+    os << "Task[" << t.name << "] priority=" << t.priority
+       << " assigned_to=" << t.assigned_to;
+    return os;
+}
+ 
  
